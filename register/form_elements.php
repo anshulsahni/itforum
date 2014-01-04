85e1408f47d72af0677820bc9020ce62e9c04ccb
anshulsahni <anshul_sahni@rediffmail.com>
2014-01-04 23:49:44 +0530

    designed the html structure for registering

---
+++ register/form_elements.php
@@ -0,0 +1,26 @@
+<?php
+	class form_elements
+	{
+
+		
+		public function drawTextBox($name,$id=NULL,$class=NULL,$placeholder=NULL,$maxlength=NULL)
+		{echo "<input type='text' name='".$name."' id='".$id."' class='".$class."' placeholder='".$placeholder."' maxlength=".$maxlength."/>";}
+
+		public function drawPassword($name,$id=NULL,$class=NULL,$placeholder=NULL,$maxlength=NULL)
+		{echo "<input type='password' name='".$name."' id='".$id."' class='".$class."' placeholder='".$placeholder."' maxlength=".$maxlength."/>";}
+
+
+		public function drawSubmit($name,$value,$id,$class,$onclick)
+		{echo "<input type='submit' name='".$name."' value='".$value."' id='".$id."' class='".$class."' onClick='".$onclick."'/>";}
+
+		public function drawDropDown($name,$id=NULL,$class=NULL,$options)
+		{
+			echo "<select name='$name' id='$id' class='$class'/>";
+			foreach ($options as $op)
+			{echo "<option value='$op[0]'>$op[1]</option>";}
+		}
+
+	}
+
+
+?>
--- register/index.php
+++ register/index.php
@@ -1,1 +1,61 @@
-//index file for registeration
+<?php
+include('./form_elements.php');
+$it_register = new form_elements();		//object to build the elements for the form 
+//options set for the drop down of sections
+$section_op= Array 						
+(
+	Array('','Select Section...'),
+	Array('A','A'),
+	Array('B','B'),
+	Array('C','C'),
+	Array('D','D'),
+	Array('E','E')
+);
+?>
+<html>
+<head>
+	<title>IT Association Forum Registration</title>
+	<link rel="stylesheet" type="text/css" href="./css/index.css">
+</head>
+<body>
+	<div id='wrapper'>
+		<?php require_once('../include/header.inc'); ?>
+		<div id='container'>
+			<div id='register_form_holder'>
+				<form id='register_form' class='form' action='register_verify.php' name='register_form'>
+					<table border=0>
+						<tr>
+							<td><?php $it_register->drawTextBox('reg_no','reg_no_text','register_input textbox','Register Number',10);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawPassword('user1','user1_text','register_input textbox password','Password',15);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawPassword('user2','user2_text','register_input textbox password','Enter Password Again',15);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawTextBox('email','email_text','register_input textbox','E Mail',50);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawTextBox('name','name_text','register_input textbox','Name',30);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawDropDown('section','section_text','register_input drop_down',$section_op);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawTextBox('hometown','hometown_text','register_input textbox','Home Town',30);?></td>
+						</tr>
+						<tr>
+							<td><?php $it_register->drawTextBox('number','number_text','register_input textbox','Mobile Number',10);?></td>
+						</tr>
+					</table>
+					<div id='submit_wrapper' class='btn_wrapper'>
+						<?php $it_register->drawSubmit('register_submit','SUBMIT','register_submit_btn','btn submit_btn','return detVerify()');?>
+					</div>
+				</form>
+			</div>
+		</div>
+		<?php require_once('../include/footer.inc'); ?>
+	</div>
+</body>
+</html>
